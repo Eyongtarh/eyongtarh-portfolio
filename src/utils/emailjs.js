a77ef482ea7e613d971657faed6bfb976541ed8d
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2026-07-18 01:02:33 +0200

    add finalise emailjs hooks and contact javascript and css to fix it

--- src/utils/emailjs.js
+++ src/utils/emailjs.js
@@ -1,20 +1,18 @@
 import emailjs from "@emailjs/browser";
 
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
-export async function sendEmail(data) {
+export async function sendEmail(formData) {
   return emailjs.send(
     SERVICE_ID,
     TEMPLATE_ID,
     {
-      from_name: data.name,
-      from_email: data.email,
-      subject: data.subject,
-      message: data.message,
+      name: formData.name,
+      email: formData.email,
+      title: formData.subject,
+      message: formData.message,
     },
     PUBLIC_KEY,
   );
