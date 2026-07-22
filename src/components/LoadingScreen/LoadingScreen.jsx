ad198efa537683ef6e0be5494a7e2d6c9e38a643
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2026-07-22 06:37:57 +0200

    edit loader image

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -5,21 +5,22 @@ function LoadingScreen({ isLoading }) {
     <div className={`loading-screen ${!isLoading ? "hidden" : ""}`}>
       <div className="loader-content">
         <div className="loader-logo">
-          <span>
-            {" "}
-            <img
-              src="/profile.jpeg"
-              alt="Eyongtarh Besong"
-              width="90"
-              height="90"
-              loading="eager"
-              fetchPriority="high"
-              decoding="async"
-            />
-          </span>
+          <img
+            className="loader-image"
+            src="/profile.jpeg"
+            alt="Eyongtarh Besong"
+            width="90"
+            height="90"
+            loading="eager"
+            fetchPriority="high"
+            decoding="async"
+          />
         </div>
+
         <h2>Eyongtarh Besong</h2>
+
         <p>Building amazing experiences...</p>
+
         <div className="loading-bar">
           <span></span>
         </div>
